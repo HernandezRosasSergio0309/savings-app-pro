542293b61564ff870ccc3b3d739a8d98f5549dd6
HernandezRosasSergio0309 <hernandez.rosas.sergio.m3s2@gmail.com>
2026-02-22 01:28:25 -0600

    Create detailed report for user savings goals
    Added a detailed SQL report for user savings goals.

--- queries/report_savings.sql
+++ queries/report_savings.sql
@@ -1,1 +1,13 @@
+-- Report: Detailed list of user goals
+-- This joins Users, Goals, and Frequencies
 
+SELECT 
+    u.username AS 'User',
+    sg.goal_name AS 'Goal',
+    sg.target_amount AS 'Target',
+    f.frequency_name AS 'Save Interval',
+    sg.start_date AS 'Starts',
+    sg.end_date AS 'Deadline'
+FROM users u
+INNER JOIN savings_goals sg ON u.user_id = sg.user_id
+INNER JOIN frequencies f ON sg.frequency_id = f.frequency_id;
